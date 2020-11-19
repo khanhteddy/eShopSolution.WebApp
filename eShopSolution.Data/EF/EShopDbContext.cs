6f819b48fbc7a0d334700e4ccb4a710f99614bfb
khanhnc <khanhnguyen147369@gmail.com>
2020-11-20 00:45:25 +0700

    Add Entity

---
+++ eShopSolution.Data/EF/EShopDbContext.cs
@@ -0,0 +1,46 @@
+﻿using eShopSolution.Data.Configurations;
+using eShopSolution.Data.Entities;
+using eShopSolution.Data.Extensions;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShopSolution.Data.EF
+{
+    public class EShopDbContext : IdentityDbContext<AppUser, AppRole, Guid>
+    {
+        public EShopDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<AppConfig> AppConfigs { get; set; }
+
+        public DbSet<Cart> Carts { get; set; }
+
+        public DbSet<CategoryTranslation> CategoryTranslations { get; set; }
+        public DbSet<ProductInCategory> ProductInCategories { get; set; }
+
+        public DbSet<Contact> Contacts { get; set; }
+
+        public DbSet<Language> Languages { get; set; }
+
+        public DbSet<Order> Orders { get; set; }
+
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+        public DbSet<ProductTranslation> ProductTranslations { get; set; }
+
+        public DbSet<Promotion> Promotions { get; set; }
+
+        public DbSet<Transaction> Transactions { get; set; }
+
+        public DbSet<ProductImage> ProductImages { get; set; }
+
+        public DbSet<Slide> Slides { get; set; }
+    }
+}
---
+++ eShopSolution.Data/Entities/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace eShopSolution.Data.Entities
+{
+    public class Product
+    {
+        public int Id { set; get; }
+        public decimal Price { set; get; }
+        public int OriginalPrice { set; get; }
+        public int Stock { set; get; }
+        public DateTime DateCreated { set; get; }
+        public string SeoAlias { set; get; }
+    }
+}
